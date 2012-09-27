a8057e7803260ad108b7ec6296f9cebb8657a397
Brock York <brock@localhost.localdomain>
2012-09-27 19:47:59 +0800

    added everything

---
+++ src/prototype/Prototype.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype;
+
+/**
+ *
+ * @author brock
+ */
+public class Prototype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainWindow theWindow = new MainWindow();
+        theWindow.setTitle("OPIG PDF Collaboration Prototype 1");
+        theWindow.setVisible(true);
+    }
+}
